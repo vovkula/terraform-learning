d8540a60ecdc2ab3807f1eb63749a32f1f6fb96a
dmitry.vovkula <dmitry.vovkula@form.com>
2023-05-01 12:07:26 +0300

    print self link

--- gcp-1/outputs.tf
+++ gcp-1/outputs.tf
@@ -5,3 +5,7 @@ output "vpc_id" {
 output "vpc_gw" {
   value = data.google_compute_network.dev-ops-sbx-vpc.gateway_ipv4
 }
+
+output "vpc_self_link" {
+  value = data.google_compute_network.dev-ops-sbx-vpc.self_link
+}
